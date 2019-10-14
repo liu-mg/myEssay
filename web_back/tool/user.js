05968507de4390cf9eb02068e60d95495471d89e
刘梦歌 <177707406@qq.com>
2019-10-14 10:20:32 +0800

    第四次修改

--- web_back/tool/user.js
+++ web_back/tool/user.js
@@ -2,16 +2,56 @@
 //    - 将页面中的功能提取到user.js后需要区别哪部分代码是与接口有关的，哪部分代码是与页面有关的
 //      - 与接口有关的要留下，与页面有关的部分再放回到页面中，并通过传参方式传入
 var user = {
- // login方法用来管理用户登录接口的操作方式
- login : function(options){
-   $.ajax({
-     type : 'post',
-     url : USER_LOGIN,
-     data : options.data,
-     success : options.callback
-    //  success : function(res){
-    //    options.callback(res);
-    //  }
-   });
- }
+  // login方法用来管理用户登录接口的操作方式
+  login: function (options) {
+    $.ajax({
+      type: 'post',
+      url: USER_LOGIN,
+      data: options.data,
+      success: options.callback
+      //  success : function(res){
+      //    options.callback(res);
+      //  }
+    });
+  },
+
+  // logout方法用来管理用户退出接口的操作方式
+  logout: function (options) {
+    $.ajax({
+      type: 'post',
+      url: USER_LOGOUT,
+      success: options.callback
+
+    })
+  },
+
+  // getInfo方法用来管理用户的简单信息获取接口的操作方式
+  getInfo: function (options) {
+    $.ajax({
+      // type : 'get',
+      url: USER_INFO,
+      success: options.callback
+    });
+  },
+
+  // getAllInfo方法用来管理用户的详细信息接口的操作方式
+  getAllInfo: function (options) {
+    $.ajax({
+      url: USER_INFO_GET,
+      success: options.callback
+    })
+  },
+
+  // edit方法用来管理用户的编辑接口的操作方式
+  edit: function (options) {
+    $.ajax({
+      type: 'post',
+      url: USER_INFO_EDIT,
+      contentType: false,
+      processData: false,
+      success: options.callback
+    })
+
+  },
+
 };
